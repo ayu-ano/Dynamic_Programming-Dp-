92d22376c9d546202d3b1d0814665bfbdf2f71b2
Ayuano_Koen <ayushkumar16871@gmail.com>
2024-08-20 04:17:36 +0530

    print LCS

---
+++ 3-1_LCS_Problem/print_Lcs/print_LCS.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <iostream>
+#include <vector>
+#include <string>
+
+
+class Solution {
+public:
+    // Function to find the length of the longest common subsequence using bottom-up DP
+    string print_lcs(string str1, string str2) {
+        int n = str1.length();
+        int m = str2.length();
+
+        // Create a 2D DP array to store the length of LCS for subproblems
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+        // Fill the DP table
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                // If characters match, take the diagonal value and add 1
+                if (str1[i - 1] == str2[j - 1]) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    // If characters don't match, take the maximum of top and left values
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        // // The final value at dp[n][m] will be the length of the longest common subsequence
+        // return dp[n][m];
+
+        int i=n;
+        int j =m;
+        string ans ="";
+        while(i>0 && j>0){
+            if(str1[i-1]==str2[j-1]){
+                ans.push_back(str1[i-1]);
+                i--;
+                j--;
+            }
+            else{
+                if(dp[i-1][j]>dp[i][j-1]){
+                    i--;
+                }
+                else{
+                    j--;
+                }
+            }
+        }
+        cout << "without reverse string ans : "<< ans << endl;
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
+
+// Main function with hardcoded values for testing the bottom-up LCS
+int main() {
+    Solution sol;
+
+    // Example hardcoded input strings
+    string str1 = "AGGTAB";
+    string str2 = "GXTXAYB";
+
+    // Call the bottom-up LCS function and display the result
+    string result = sol.print_lcs(str1, str2);
+    cout << "Length of Longest Common Subsequence is: " << result << endl;
+
+    return 0;
+}
